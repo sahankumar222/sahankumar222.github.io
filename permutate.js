e78e259ddd090db456a05e2077e8f52a28e7b7cf
sahankumar222 <sahankumar222@gmail.com>
2023-01-18 19:02:25 +0530

    new one added

---
+++ permutate.js
@@ -0,0 +1,17 @@
+const stringPermutations = (str) => {
+    if (str.length === 0) return "";
+    if (str.length === 1) return str;
+
+    let result = [];
+
+    for (let i = 0; i < str.length; i++) {
+        const currentChar = str[i];
+        const remainingChars = str.slice(0, i) + str.slice(i + 1);
+        for (let j = 0; j < remainingChars.length; j++) {
+            result.push(currentChar + stringPermutations(remainingChars)[j]);
+        }
+    }
+
+    return result;
+};
+console.log(stringPermutations('abcde'));
